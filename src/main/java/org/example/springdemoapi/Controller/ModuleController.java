fccbeb9343f3faa7d71ca3df9b4c55dc813483f4
Tran Kien Phong <kienphongtran2003@gmail.com>
2025-07-29 15:53:56 +0700

    config spring security

---
+++ src/main/java/org/example/springdemoapi/Controller/ModuleController.java
@@ -0,0 +1,20 @@
+package org.example.springdemoapi.Controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class ModuleController {
+
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello World";
+    }
+    
+    @GetMapping("/get-home")
+    public String getHome(){
+        return "i want to go home";
+    }
+}
